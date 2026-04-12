2697f8c66725dcfedb129df0729226092614cb2c
Kirill <Kirill.s.anikin@gmail.com>
2026-04-12 08:00:28 -0400

    Add BrowserRouter with basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
     <HelmetProvider>
       <App />
     </HelmetProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
